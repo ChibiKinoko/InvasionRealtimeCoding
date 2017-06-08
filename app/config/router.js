997861c27accb4485c2cd02cbca00f46d2b2b1dd
laure <laure.meng@gmail.com>
2017-06-08 15:07:57 +0200

    flash workiiiiiiing !!!!!

--- app/config/router.js
+++ app/config/router.js
@@ -1,24 +1,31 @@
 module.exports = function(app, passport) {
     var path = require('path');
     var user = require('../controllers/UserController');
-    var bodyParser = require('body-parser');
-	var flash = require('connect-flash');
+
+    // var bodyParser = require('body-parser');
+    // var flash = require('connect-flash');
 
 
-    require('./strategies/local')(passport);
+    // require('./strategies/local')(passport);
 
 
     app.get('/', function(req, res) {
-        res.render('index');
+        res.render('index.jade');
+    });
+
+    app.get('/home', isLoggedIn, function(req, res) {
+        res.render('home.jade');
     });
 
     app.get('/login', function(req, res) {
-    	console.log(req.flash('loginMessage'));
-        res.render('login2', {message: req.flash('loginMessage')});
-    })
+        //console.log('ça marche ? : ' + req.flash('loginMessage'));
+        //var error = req.flash('loginMessage');
+        //console.log(error);
+        res.render('login.jade', { message: req.flash('loginMessage') });
+    });
 
     app.get('/register', function(req, res) {
-        res.render('register', {message: req.flash('signupMessage')});
+        res.render('register.jade', { message: req.flash('signupMessage') });
     });
 
     app.post('/login', passport.authenticate('login', {
@@ -28,14 +35,29 @@ module.exports = function(app, passport) {
     }));
 
     app.post('/register', passport.authenticate('signup', {
-        successRedirect: '/home',
+        successRedirect: '/login',
         failureRedirect: '/register',
         failureFlash: true
     }));
 
-     app.get('/home', function(req, res) {
-        res.render('home');
+
+    // =====================================
+    // LOGOUT ==============================
+    // =====================================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
     });
 
+}
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
 
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+    else 
+		// if they aren't redirect them to the home page
+		// res.end('failed');	
+		res.redirect('/');
 }
--- server.js
+++ server.js
@@ -1,42 +1,106 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
 
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var cookieparser = require("cookieparser");
-var flash = require('connect-flash');
-var session = require('express-session');
+// var server = require('http').Server(app);
+// var io = require('socket.io')(server);
+
+// var mongoose = require(__dirname + '/app/config/db.js');
+// var passport = require('passport');
+
+// var flash = require('connect-flash');
+// var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
+// var session = require('express-session');
+
+
+// // configuration ===============================================================
+// var app = express();
+
+// // templating render
+// // app.set('view engine', 'ejs')
+// app.set('views', __dirname + '/app/views');
+// app.set('view engine', 'jade')
+
+// app.use(express.static(__dirname + '/public'));
+// app.use(cookieParser());
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+// app.use(session({
+//   secret: 'Invasion of Cats',
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: { secure: true }
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session()); // persistent login sessions
+// app.use(flash());
+
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: {},
+//   });
+// });
+
+
+// // routes ======================================================================
+// require('./app/config/strategies/local')(passport);
+// require(__dirname + '/app/config/router')(app, passport);
+// // server file
+// require(__dirname + '/app/app')(io);
+
+// // launch ======================================================================
+// server.listen(3000, function() {
+// 	console.log('The magic happens on port 3000');
+// });
+
+
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var mongoose = require(__dirname + '/app/config/db.js');
+var port = process.env.PORT || 3000;
+
 var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
-// configuration ===============================================================
+var mongoose = require('mongoose');
+var flash = require('connect-flash');
+var session = require('express-session');
 
-// templating render
-app.set('view engine', 'jade')
+var app = express();
+// var routes = require('./routes/index');
+
+var configDB = require('./app/config/db.js');
+
+app.set('view engine', 'jade');
 app.set('views', __dirname + '/app/views');
 
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(logger('dev')); //log in console
 app.use(bodyParser.json());
-app.use(session({
-  secret: 'Invasion of Cats',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true }
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({ secret: 'shhsecret' }));
 app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
+app.use(passport.session());
 app.use(flash());
 
-// routes ======================================================================
+require('./app/config/passport')(passport);
+require('./app/config/router')(app, passport);
 
-require(__dirname + '/app/config/router')(app, passport);
-// server file
-require(__dirname + '/app/app')(io);
-
-// launch ======================================================================
-server.listen(3000, function() {
-	console.log('The magic happens on port 3000');
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+  });
 });
+
+app.listen(port);
+
+// module.exports = app;
+
